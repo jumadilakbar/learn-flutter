5a0756c9eca9371ed75776f805a9bfb0801e9613
jumadilakbar <muhamadjumadilakbar@gmail.com>
2023-11-09 23:25:30 +0700

    extract widget

---
+++ lib/trial_basic_extract_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "my APP",
+              style: TextStyle(color: Colors.black),
+            ),
+            backgroundColor: Colors.amber,
+          ),
+          body: GridView(
+            padding: EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                childAspectRatio: 1 / 2),
+            children: [
+              ThemeTextColor(
+                text: "Yellow",
+                bycolor: Colors.yellow,
+              ),
+              ThemeTextColor(
+                text: "Purple",
+                bycolor: Colors.deepPurpleAccent,
+              ),
+              Container(
+                color: Colors.blueAccent,
+              ),
+              Container(
+                color: Colors.pink,
+              ),
+              Container(
+                color: Colors.deepOrangeAccent,
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+class ThemeTextColor extends StatelessWidget {
+  const ThemeTextColor({
+    Key? key,
+    required this.text,
+    required this.bycolor,
+  }) : super(key: key);
+
+  final String text;
+  final Color bycolor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      color: bycolor,
+      child: Center(child: Text(text)),
+    );
+  }
+}
